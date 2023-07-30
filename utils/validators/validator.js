9d7a22a81b73baab1bb4938b4203346ab642f325
Mila Petruk <mila.petruk@gmail.com>
2023-07-30 14:20:54 +0200

    updated controllers and schema

--- utils/validators/validator.js
+++ utils/validators/validator.js
@@ -4,6 +4,7 @@ const contactSchema = joi.object({
 	name: joi.string().min(3), //NAME SHOULD HAVE MINIMUM 3 SYMBOLS
 	email: joi.string().email(),//EMAIL SHOULD BE IN CORRECT FORMAT
 	phone: joi.string().min(5),//PHONE SHOULD HAVE AT LEAST 5 SYMBOLS
+	favorite: joi.boolean(),//VALIDATE FAVORITE CONTACT FROM THE LIST
 })
 
 const validator = schema => body => {
